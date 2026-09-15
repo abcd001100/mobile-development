e0b42d40d90028e8e4827864b9dec9fb08d5f1a5
Abdulaziz Taju <abdulaziztaju42@gmail.com>
2026-09-15 22:34:25 +0800

    Add module 4 codes

---
+++ Module-4/Source Code/03_string_templates.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val email     = 100
+    val notif     = 30
+    val reminders = 10
+
+    println("$email emails, $notif notifications, $reminders reminders")
+
+    val unread = notif
+    println("You have $unread unread messages.")
+}
